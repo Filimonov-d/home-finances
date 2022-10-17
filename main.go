abeb91b7cab6641a9e4f79e13099fd8e8f591d61
Filimonov-d <filimonov.d.ua@gmail.com>
2022-10-17 22:49:43 +0300

    add profit test

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"fmt"
+	//"github.com/gin-gonic/gin"
+)
+
+type Profit struct {
+	Amount int
+	Source string
+	Date   string
+}
+
+func InputProfit(p Profit) {
+	fmt.Println("Enter amount(int):")
+	fmt.Scanln(p.Amount)
+
+	fmt.Println("Enter Source(string):")
+	fmt.Scanln(p.Source)
+
+	fmt.Println("Enter Date(string):")
+	fmt.Scanln(p.Date)
+
+	fmt.Println("Ur Amount: ", p.Amount)
+	fmt.Println("Ur Source: ", p.Source)
+	fmt.Println("Ur Date: ", p.Date)
+
+}
+
 func main() {
 
+	InputProfit(Profit{})
+
+	/* router := gin.Default()
+	router.GET("/track/listen", listenTrack)
+	router.GET("/albums/listen", listenAlbums)
+
+	router.Run("localhost:8080")
+	fmt.Println("s") */
 }
