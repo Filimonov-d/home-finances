f84ba68bbd3cb51787b89ad553a6e47114f9e31a
Filimonov-d <filimonov.d.ua@gmail.com>
2022-10-20 20:03:42 +0300

    add month income

--- main.go
+++ main.go
@@ -11,25 +11,66 @@ type Profit struct {
 	Date   string
 }
 
-func InputProfit(p Profit) {
-	fmt.Println("Enter amount(int):")
-	fmt.Scanln(p.Amount)
+type Income struct {
+	Amount int
+}
 
-	fmt.Println("Enter Source(string):")
-	fmt.Scanln(p.Source)
+var PointerProfit = Profit{}
+var PointerIncome = Income{}
 
-	fmt.Println("Enter Date(string):")
-	fmt.Scanln(p.Date)
+func InputAmount(p *Profit) {
 
-	fmt.Println("Ur Amount: ", p.Amount)
-	fmt.Println("Ur Source: ", p.Source)
-	fmt.Println("Ur Date: ", p.Date)
+	fmt.Println("\nEnter Amount(integer):")
+	if _, err := fmt.Scanln(&p.Amount); err != nil {
+		fmt.Println("Amount error: ", err)
+		InputAmount(&PointerProfit)
+	}
+}
 
+func InputSource(p *Profit) {
+
+	fmt.Println("\nEnter Source(string):")
+	if _, err := fmt.Scanln(&p.Source); err != nil {
+		fmt.Println("Source error: ", err)
+		InputSource(&PointerProfit)
+	}
+}
+
+func InputDate(p *Profit) {
+
+	fmt.Println("\nEnter Date(string):")
+	if _, err := fmt.Scanln(&p.Date); err != nil {
+		fmt.Println("Date error: ", err)
+		InputDate(&PointerProfit)
+	}
+}
+func InputProfit(p *Profit) {
+
+	InputAmount(&PointerProfit)
+	defer fmt.Println("Ur Amount: ", p.Amount)
+
+	InputSource(&PointerProfit)
+	defer fmt.Println("Ur Source: ", p.Source)
+
+	InputDate(&PointerProfit)
+	defer fmt.Println("\nUr Date: ", p.Date)
+
+}
+
+func InputIncome(i *Income) {
+
+	fmt.Println("Enter Amount(integer):")
+	if _, err := fmt.Scanln(&i.Amount); err != nil {
+		fmt.Println("Amount error: ", err)
+		InputIncome(&PointerIncome)
+	}
 }
 
 func main() {
 
-	InputProfit(Profit{})
+	InputIncome(&PointerIncome)
+
+	InputProfit(&PointerProfit)
 
 	/* router := gin.Default()
 	router.GET("/track/listen", listenTrack)
