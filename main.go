9aee821226bebb807ebd93f7c5fb4529cdfad1d8
Filimonov-d <filimonov.d.ua@gmail.com>
2022-11-07 20:41:30 +0100

    gin init

--- main.go
+++ main.go
@@ -2,174 +2,34 @@ package main
 
 import (
 	"fmt"
-	//"github.com/gin-gonic/gin"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Profit struct {
-	Amount int
-	Source string
-	Date   string
+	Amount int    `json:"amount"`
+	Source string `json:"source"`
+	Date   string `json:"date"`
 }
 
-type Income struct {
-	Amount int
-}
+func InsertProfit(c *gin.Context) {
 
-func InputAmount(p *Profit) {
-	var PointerProfit = Profit{}
+	fmt.Println("Inserting Profit")
 
-	fmt.Println("\nEnter Amount(integer):")
-	if _, err := fmt.Scanln(&p.Amount); err != nil {
-		fmt.Println("Amount error: ", err)
-		InputAmount(&PointerProfit)
+	var profit Profit
+	if err := c.ShouldBindJSON(&profit); err != nil {
+		c.String(http.StatusBadRequest, err.Error())
 	}
-}
 
-func InputSource(p *Profit) {
-	var PointerProfit = Profit{}
-
-	fmt.Println("\nEnter Source(string):")
-	if _, err := fmt.Scanln(&p.Source); err != nil {
-		fmt.Println("Source error: ", err)
-		InputSource(&PointerProfit)
-	}
-}
-
-func InputDate(p *Profit) {
-	var PointerProfit = Profit{}
-
-	fmt.Println("\nEnter Date(string):")
-	if _, err := fmt.Scanln(&p.Date); err != nil {
-		fmt.Println("Date error: ", err)
-		InputDate(&PointerProfit)
-	}
-}
-func InputProfit(p *Profit) {
-	var PointerProfit = Profit{}
-
-	InputAmount(&PointerProfit)
-	defer fmt.Println("Ur Amount: ", p.Amount)
-
-	InputSource(&PointerProfit)
-	defer fmt.Println("Ur Source: ", p.Source)
-
-	InputDate(&PointerProfit)
-	defer fmt.Println("\nUr Date: ", p.Date)
-
-}
-
-func InputIncome(i *Income) {
-	var PointerIncome = Income{}
-
-	fmt.Println("Enter Amount(integer):")
-	if _, err := fmt.Scanln(&i.Amount); err != nil {
-		fmt.Println("Amount error: ", err)
-		InputIncome(&PointerIncome)
-	}
-}
-
-func main() {
-	var PointerProfit = Profit{}
-	var PointerIncome = Income{}
-
-	InputIncome(&PointerIncome)
-
-	InputProfit(&PointerProfit)
-
-	/* router := gin.Default()
-	router.GET("/track/listen", listenTrack)
-	router.GET("/albums/listen", listenAlbums)
-
-	router.Run("localhost:8080")
-	fmt.Println("s") */
-}
-
-/*package main
-
-import (
-	"fmt"
-	//"github.com/gin-gonic/gin"
-)
-
-type Profit struct {
-	Amount int
-	Source string
-	Date   string
-}
-
-type Income struct {
-	Amount int
-}
-
-func InputAmount(p *Profit) {
-	var PointerProfit = Profit{}
-
-	fmt.Println("\nEnter Amount(integer):")
-	if _, err := fmt.Scanln(&p.Amount); err != nil {
-		fmt.Println("Amount error: ", err)
-		InputAmount(&PointerProfit)
-	}
-}
-
-func InputSource(p *Profit) {
-	var PointerProfit = Profit{}
-
-	fmt.Println("\nEnter Source(string):")
-	if _, err := fmt.Scanln(&p.Source); err != nil {
-		fmt.Println("Source error: ", err)
-		InputSource(&PointerProfit)
-	}
-}
-
-func InputDate(p *Profit) {
-	var PointerProfit = Profit{}
-
-	fmt.Println("\nEnter Date(string):")
-	if _, err := fmt.Scanln(&p.Date); err != nil {
-		fmt.Println("Date error: ", err)
-		InputDate(&PointerProfit)
-	}
-}
-func InputData(p *Profit) {
-	var PointerProfit = Profit{}
-
-	InputAmount(&PointerProfit)
-	defer fmt.Println("Profit Amount: ", p.Amount)
-
-	InputSource(&PointerProfit)
-	defer fmt.Println("Profit Source: ", p.Source)
-
-	InputDate(&PointerProfit)
-	defer fmt.Println("\nProfit Date: ", p.Date)
-	defer fmt.Println("\nUr Profit: ")
-
-}
-
-func InputIncome(i *Income) {
-	var PointerIncome = Income{}
-
-	fmt.Println("Enter Amount(integer):")
-	if _, err := fmt.Scanln(&i.Amount); err != nil {
-		fmt.Println("Amount error: ", err)
-		InputIncome(&PointerIncome)
-	}
-	defer fmt.Println("\nMonth income amount: ", i.Amount)
-
+	fmt.Println(profit)
 }
 
 func main() {
 
-	var PointerProfit = Profit{}
-	var PointerIncome = Income{}
-
-	p := InputDate(Profit{})
-	InputIncome(&PointerIncome)
-
-	InputData(&PointerProfit)
-
-	/* router := gin.Default()
-	router.GET("/track/listen", listenTrack)
-	router.GET("/albums/listen", listenAlbums)
+	router := gin.Default()
+	router.POST("/profit/insert", InsertProfit)
 
 	router.Run("localhost:8080")
-	fmt.Println("s") }*/
+	fmt.Println("s")
+}
